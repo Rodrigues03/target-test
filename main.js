7e52ecbecaff7fc21bbd002baaf79eef58490b44
Ericson Rodrigues <56169844+Rodrigues03@users.noreply.github.com>
2023-04-10 18:38:52 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import reverseWord from './scripts/reverseWord.js';
+import Questao2 from './scripts/Questao2.js';
+import Questao1 from './scripts/Questao1.js';
+
+Questao1();
+Questao2();
+reverseWord();
